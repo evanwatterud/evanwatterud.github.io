7e16d172ab316c9cb14028a18f10d7553a32f0b1
evanwatterud <cod.videos@hotmail.com>
2018-04-30 19:39:17 -0400

    raindrops falling properly

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import P5Wrapper from '../components/P5Wrapper'
+import purpleRain from '../sketches/purpleRain'
 
 class HomePage extends React.Component {
   render () {
     return (
-      null
+      <div>
+        <P5Wrapper sketch={purpleRain} />
+      </div>
     )
   }
 }
---
+++ src/sketches/purpleRain.js
@@ -0,0 +1,57 @@
+export default function purpleRain (p) {
+  let rainDrops = []
+  const NUM_DROPS = 400
+
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
+    p.frameRate(120)
+    createDrops()
+  }
+
+  function createDrops() {
+    for (var i = 0; i < NUM_DROPS; i++) {
+      rainDrops[i] = new RainDrop(getRandomFloat(-p.windowWidth/2, p.windowWidth/2),
+                                  getRandomFloat(3 * -p.windowHeight, -p.windowHeight/2 - 1),
+                                  getRandomFloat(2, 4))
+    }
+  }
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    // Called if the wrapper props change
+  }
+
+  p.draw = function () {
+    p.background(208, 202, 209)
+    for (var i = 0; i < rainDrops.length; i++) {
+      rainDrops[i].fall()
+      rainDrops[i].show()
+    }
+  }
+
+  class RainDrop {
+    constructor(x, y, distance) {
+      this.x = x
+      this.y = y
+      this.length = (distance/2) * 10
+      this.fallSpeed = distance
+    }
+
+    fall() {
+      if (this.y + this.fallSpeed > p.windowHeight/2 + 50) {
+        this.y = getRandomFloat(3 * -p.windowHeight, -p.windowHeight/2 - 1)
+      } else {
+        this.y = this.y + this.fallSpeed
+      }
+    }
+
+    show() {
+      p.stroke(153, 0, 204)
+      p.strokeWeight(2)
+      p.line(this.x, this.y, this.x, this.y + this.length)
+    }
+  }
+}
+
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
