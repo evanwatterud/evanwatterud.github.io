9c45de8e985d88c9f45c330c41b199b25b137683
evanwatterud <cod.videos@hotmail.com>
2018-05-29 18:44:49 -0400

    added color transition and more tilt effect to project cards

--- src/components/GitHubProjects.js
+++ src/components/GitHubProjects.js
@@ -7,18 +7,11 @@ import VanillaTilt from 'vanilla-tilt'
 class GitHubProjects extends React.Component {
   constructor(props) {
     super(props)
-    this.projects = []
   }
 
-  componentWillMount() {
-    let url = 'https://github.com/evanwatterud'
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => console.log('API Error'))
+  onProjectClicked = (projectName) => {
+    var win = window.open(`https://github.com/evanwatterud/${projectName}`, '_blank');
+    win.focus();
   }
 
   componentDidMount() {
@@ -26,8 +19,10 @@ class GitHubProjects extends React.Component {
     let projectElements = document.querySelectorAll(".project")
     for (var i = 0; i < projectElements.length; i++) {
       VanillaTilt.init(projectElements[i], {
-    		max: 25,
-    		speed: 400
+    		max: 30,
+    		speed: 400,
+        glare: true,
+        scale: 1.07
     	});
     }
   }
@@ -36,14 +31,48 @@ class GitHubProjects extends React.Component {
     return (
       <div id="projectsContainer">
         <div id="rowOne" className="projectRow">
-          <div className="project"></div>
-          <div className="project"></div>
-          <div className="project"></div>
+          <div className="project" onClick={() => this.onProjectClicked("NodePoker")}>
+            <h1 className="projectTitle" >NodePoker</h1>
+            <h4 className="projectDescription" >
+              A simple variation of 5 card draw poker, written with Node.js and Socket.io
+            </h4>
+          </div>
+          <div className="project" onClick={() => this.onProjectClicked("TTCFOYL")}>
+            <h1 className="projectTitle" >TTCFOYL</h1>
+            <h4 className="projectDescription" >
+              Tap The Circles Fast Or You Lose, a circle tapping game written with React Native
+              and Redux
+            </h4>
+          </div>
+          <div className="project" onClick={() => this.onProjectClicked("SpotifySongAdder")}>
+            <h1 className="projectTitle" >SpotifySongAdder</h1>
+            <h4 className="projectDescription" >
+              Tray app built with Electron to allow adding songs to Spotify library with a
+              global hotkey
+            </h4>
+          </div>
         </div>
         <div className="projectRow">
-          <div className="project"></div>
-          <div className="project"></div>
-          <div className="project"></div>
+          <div className="project" onClick={() => this.onProjectClicked("HappyTrees")}>
+            <h1 className="projectTitle" >HappyTrees</h1>
+            <h4 className="projectDescription" >
+              A drawing playground written with React Native
+            </h4>
+          </div>
+          <div className="project" onClick={() => this.onProjectClicked("TwitterSentimentAnalyzer")}>
+            <h1 className="projectTitle" >Twitter Sentiment Analyzer</h1>
+            <h4 className="projectDescription" >
+              A machine learning app that lets you know the general positivity or negativity
+              surrounding topics of your choice on twitter. Written with python and Keras using
+              Tensorflow as the backend.
+            </h4>
+          </div>
+          <div className="project" onClick={() => this.onProjectClicked("evanwatterud.github.io")}>
+            <h1 className="projectTitle" >evanwatterud.github.io</h1>
+            <h4 className="projectDescription" >
+              This website! Written with React.js.
+            </h4>
+          </div>
         </div>
       </div>
     )
