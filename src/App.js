4f3deab8e40fb21aaabb11e527e72f99620bb1e0
evanwatterud <cod.videos@hotmail.com>
2018-08-24 17:53:09 -0400

    added normalize.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './css/normalize.css'
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
